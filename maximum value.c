18a11a0496527a3cde563e4e26d8f2b54d5ed940
Apurba Roy Anik <54495133+apurba667@users.noreply.github.com>
2019-11-28 22:47:45 +0600

    add

---
+++ maximum value.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+
+
+typedef struct BST
+
+{
+
+    int data ;
+
+    struct BST *left , *right;
+
+}BST;
+
+
+
+void insert_BST(BST **root, int n)
+
+{
+
+    if((*root)==NULL)
+
+        {
+
+        BST *temp = (BST*)malloc(sizeof(BST));
+
+        temp->data=n;
+
+        temp->left=NULL;
+
+        temp->right=NULL;
+
+        *root=temp;
+
+        }
+
+    else
+
+    {
+
+        if((*root)->data>n){
+
+            insert_BST((&(*root)->left),n);
+
+        }
+
+        else {
+
+            insert_BST((&(*root)->right),n);
+
+        }
+
+    }
+
+}
+
+int main ()
+
+{
+    int maximum=0;
+
+    BST *root = NULL;
+
+    int a[7]={21,22,49,26,28,70,24};
+
+    int i;
+
+    for(i=0;i<7;i++){
+
+        insert_BST(&root,a[i]);
+
+    }
+    for(i=0;i<7;i++){
+        if(a[i]>maximum){
+            maximum=a[i];
+        }
+    }
+    printf("Maximum value = %d",maximum);
+return 0;
+}
